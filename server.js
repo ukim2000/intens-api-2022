cb00f64bf4c62adf95c9264c94c84aec5774fe17
ukim2000 <ukim2000@gmail.com>
2024-06-13 02:48:10 +0200

    Dodao github actions

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!');
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
